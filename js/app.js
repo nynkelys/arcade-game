15442a18e298046fa3d3f21a2a546688f16d1bd0
Nynke Lys de Boer <n-deboer@outlook.com>
2018-05-07 13:40:59 +0200

    Instantiate objects

--- js/app.js
+++ js/app.js
@@ -12,10 +12,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if (this.x <= 505) {
+    if (this.x <= 505) { // Enemies only move horizontally, hence only x-value changes
         this.x += this.speed * dt;
     } else {
-        this.x = 0;
+        this.x = -100;
     }
 };
 
@@ -28,16 +28,39 @@ Enemy.prototype.render = function() { // Rendering = 'weergeven'
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(x, y, speed) {
-    this.x = x; // 200
-    this.y = y; // 400
-    this.speed = speed; // 50
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/char-princess-girl.png'
+};
+
+Player.prototype.update = function() { // Add: keyboard events
+// Turn into if-else statement for boundaries of canvas
+// i.e. if this.y > nnn;
+// this.y = nnn;
+// do this for left, right and bottom
+// Add: top, if player reaches water, reset game
+};
+
+Player.prototype.render = function() { // Draw player on screen
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(e) {
+    this.ctlKey = e;
 };
 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+const player = new Player(202.5, 400, 50);
+const enemyOne = new Enemy(-100, 227.5, 50);
+const enemyTwo = new Enemy(-100, 227.5, 50);
+const enemyThree = new Enemy(-100, 145, 50);
+const enemyFour = new Enemy(-100, 60, 50);
+const enemyFive = new Enemy(-100, 60, 50);
+
+let allEnemies = [];
+allEnemies.push(enemyOne, enemyTwo, enemyThree, enemyFour, enemyFive);
 
 
 
