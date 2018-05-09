78e77b0bba81018e763e0e9a1984796ecf843b39
Nynke Lys de Boer <n-deboer@outlook.com>
2018-05-09 12:27:27 +0200

    Update scoreboard

--- js/app.js
+++ js/app.js
@@ -73,6 +73,7 @@ Player.prototype.handleInput = function(keyCode) {
         if (this.y - 85 < 0) {
             resetPlayer();
             counter = counter + 1;
+            scoreBoard.innerHTML = counter;
         } else {
             this.y -= 83;
         }
@@ -85,6 +86,8 @@ function checkCollision(oneEnemy) { // if 'this' collides with player
     player.y < oneEnemy.y + 80 &&
     40 + player.y > oneEnemy.y) {
         resetPlayer();
+        counter = 0;
+        scoreBoard.innerHTML = counter;
     }
 };
 
@@ -95,10 +98,6 @@ function resetPlayer() {
 
 let scoreBoard = document.getElementById('scoreboard');
 let counter = 0;
-
-function setScore() {
-    scoreBoard.innerText = counter;
-};
 
 // Now instantiate your objects.
 const player = new Player(202, 404); // 505 / 2 - 50.5 = 202 (0, 0 is top left corner)
